0f5ecc4d8b525b60825f5d4ef8cc7001d35ba62d
Akil-A <akil@akil.no>
2016-06-11 14:01:07 +0200

    Initial Commit

---
+++ iKalender/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iKalender
+//
+//  Created by Akil Asghar on 11.06.2016.
+//  Copyright © 2016 Mantena AS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
